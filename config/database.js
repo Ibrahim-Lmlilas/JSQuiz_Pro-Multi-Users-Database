85e578f7fa7854b54bac69a55c5e9b5de35d713c
Hakari <lmlilas.ibrahim@student.youcode.ma>
2025-10-03 11:19:34 +0100

    start task admin

--- config/database.js
+++ config/database.js
@@ -22,16 +22,6 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD || "",
   {
     host: process.env.DB_HOST || "localhost",
-    dialect: "mysql",
-    port: process.env.DB_PORT || 3306,
-  }
-);
-const sequelize = new Sequelize(
-  process.env.DB_NAME || "quiz",
-  process.env.DB_USER || "root",
-  process.env.DB_PASSWORD || "",
-  {
-    host: process.env.DB_HOST || "localhost",
     dialect: process.env.DB_DIALECT || "mysql",
     port: process.env.DB_PORT || 3306,
   }
